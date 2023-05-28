367027d82cdd02bb811655c0414cb1bcdaec4f6f
KerenRozen <kr@mail.tau.ac.il>
2023-05-28 18:14:09 +0300

    Final version Gh38 classifications

---
+++ final/example.py
@@ -0,0 +1,9 @@
+
+# Artem run guide:
+
+# In top of file:
+exec(open("run_gh38_annotation_function.py").read())
+GH38_RAW_PATH = "C:/Users/keren/PycharmProjects/genomic-annotation/data/Homo_sapiens.GRCh38.109.gff3.gz"
+
+# In code:
+match_classifications(1, 1, 2, 1)
